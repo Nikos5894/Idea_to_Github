1c1e797396ef631b978211b0f3a36b5215dbb6dc
Admin <kolia.osadchukcool@gmail.com>
2022-01-30 22:08:58 +0200

    some changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helllo world");
+        System.out.println();
         System.out.println("some changes");
     }
 }
